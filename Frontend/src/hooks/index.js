7b14889488e770ea52870569892e18f3fd2a466a
arunbharathi-parkkavamani-dev <coc.arunbharathi@gmail.com>
2026-07-24 18:02:12 +0530

    feat(frontend): add domain custom hooks layer (useTasks, usePayroll, useAttendance, useCRM, useAssets)

--- Frontend/src/hooks/index.js
+++ Frontend/src/hooks/index.js
@@ -6,3 +6,4 @@ export { useRealtimeSync } from './useRealtimeSync';
 export { useTaskMutations } from './useTaskMutations';
 export { useUserProfile } from './useUserProfile';
 export { useUserRole } from './useUserRole';
+export { useTasks, usePayroll, useAttendance, useCRM, useAssets } from './domain';
